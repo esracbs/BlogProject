0e9bad0046f6b4336c89a0814a3fa67eb3f8af8b
esracbs <65399307+esracbs@users.noreply.github.com>
2022-06-17 15:54:46 +0300

    107.ders

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            string api = "d6d8137144e059a3d8805005820740db";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            return View();
+        }
+        
+    }
+}
